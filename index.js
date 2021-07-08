c0f35e02e826bd93a72b315422483873147c8bf2
Shareena Begum <shareena.fsp@gmail.com>
2021-07-08 11:06:44 +0200

    leads array

---
+++ index.js
@@ -0,0 +1,14 @@
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+
+let myLeads= []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+// let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function() {
+    myLeads.push("www.awesomelead.com")
+    console.log(myLeads)
+})
+
