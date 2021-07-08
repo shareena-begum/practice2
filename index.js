3299b8e893b2e03c2f041041a83583a24361d1c2
Shareena Begum <shareena.fsp@gmail.com>
2021-07-08 12:48:44 +0200

    mylead push funtion by adding ul in html page

--- index.js
+++ index.js
@@ -2,13 +2,29 @@
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
 
-let myLeads= []
+let myLeads= ["www.shannasosweet.com", "www.shannasosour.com", "www.shannasobitter.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 
 // let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
-    myLeads.push("www.awesomelead.com")
+    myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for(let i=0; i < myLeads.length; i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+
+    //*** Another method***
+    // create element
+    // // set text content
+    // // append to ul
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+
+}
 
